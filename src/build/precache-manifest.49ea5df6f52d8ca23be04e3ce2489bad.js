7dde10508f9032c27b41ab09fec5980445ba2649
egheCodes <ehioroboeghe@gmail.com>
2020-02-28 13:38:31 +0100

    updating back-end files

---
+++ src/build/precache-manifest.49ea5df6f52d8ca23be04e3ce2489bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dcb4d262ce725bae44593656c20e0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cff4eb61a95b28a65f41",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9726b259c189a5d812fa",
+    "url": "/static/js/2.1cf4a199.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1cf4a199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff4eb61a95b28a65f41",
+    "url": "/static/js/main.b69aa6de.chunk.js"
+  },
+  {
+    "revision": "f34df4cc19e0da90cba3",
+    "url": "/static/js/runtime-main.578b6095.js"
+  }
+]);
